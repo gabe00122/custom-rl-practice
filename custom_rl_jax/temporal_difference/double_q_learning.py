dca15c13d332ad232ecd46c000a6811db6e3e268
Gabriel keith <gabe.keith@yahoo.com>
2023-11-21 07:33:21 -0500

    frozen lake

--- custom_rl_jax/temporal_difference/double_q_learning.py
+++ custom_rl_jax/temporal_difference/double_q_learning.py
@@ -2,7 +2,7 @@ import gymnasium as gym
 import numpy as np
 from typing_extensions import TypedDict
 
-Policy = TypedDict('Policy', {'Q': np.ndarray, 'exploration': float, 'learning_rate': float, 'discount': float,
+Policy = TypedDict('Policy', {'Q1': np.ndarray, 'Q2': np.ndarray, 'exploration': float, 'learning_rate': float, 'discount': float,
                               'state_space': int,
                               'action_space': int})
 
@@ -10,7 +10,8 @@ Policy = TypedDict('Policy', {'Q': np.ndarray, 'exploration': float, 'learning_r
 def create_policy(state_space: int, action_space: int, exploration: float, learning_rate: float,
                   discount: float) -> Policy:
     return {
-        'Q': np.zeros((state_space, action_space)),
+        'Q1': np.zeros((state_space, action_space)),
+        'Q2': np.zeros((state_space, action_space)),
         'exploration': exploration,
         'learning_rate': learning_rate,
         'discount': discount,
@@ -19,37 +20,40 @@ def create_policy(state_space: int, action_space: int, exploration: float, learn
     }
 
 
-def act(policy: Policy, state: int) -> int:
+def act(policy: Policy, q: np.ndarray, state: int) -> int:
     if np.random.uniform() < policy['exploration']:
         return np.random.randint(policy['action_space'])
     else:
-        return np.argmax(policy['Q'][state])
+        return np.argmax(q[state])
 
 
 def train_episode(policy: Policy, env: gym.Env):
+    q1 = policy['Q1']
+    q2 = policy['Q2']
+    if np.random.uniform() > 0.5:
+        q1, q2 = q2, q1
+
     rewards = 0
-    q = policy['Q']
     learning_rate = policy['learning_rate']
     discount = policy['discount']
-    exploration = policy['exploration']
     done = False
 
     obs, _ = env.reset()
-    action = act(policy, obs)
+    action = act(policy, q1, obs)
+    q1, q2 = q2, q1
 
     while not done:
         next_obs, reward, terminated, truncated, _ = env.step(action)
 
-        greedy_return = np.max(q[next_obs])
-        exploratory_return = np.mean(q[next_obs])
-        expected_return = (greedy_return * (1.0 - exploration)) + (exploratory_return * exploration)
-
-        td_error = reward + (discount * expected_return - q[obs, action])
-        q[obs, action] += learning_rate * td_error
+        action_index = np.argmax(q1[next_obs])
+        td_error = reward + (discount * q2[next_obs, action_index] - q2[obs, action])
+        q2[obs, action] += learning_rate * td_error
 
         done = terminated or truncated
         obs = next_obs
-        action = act(policy, obs)
+        action = act(policy, q1, obs)
+        q1, q2 = q2, q1
+
         rewards += reward
 
     return rewards
