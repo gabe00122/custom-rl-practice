94f7fefb20d9b56caa4196708457c47c725bad33
Gabriel keith <gabe.keith@yahoo.com>
2023-11-27 08:35:54 -0500

    hyper parameter search

--- custom_rl_jax/nstep_temporal_difference/test_nstep.py
+++ custom_rl_jax/nstep_temporal_difference/test_nstep.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
+from policy import Policy
 from nstep_sarsa import NStepSarsa
 from tree_backup import TreeBackup
 
@@ -18,16 +19,30 @@ def main():
         "steps": 2
     })
 
-    episodes = 100000
-    rewards = np.zeros((episodes, ))
+    data = {}
+    for step in range(1, 5):
+        policy = TreeBackup({
+            "action_space": env.action_space.n,
+            "observation_space": env.observation_space.n,
+            "discount": 0.99,
+            "exploration": 0.05,
+            "learning_rate": 0.1,
+            "steps": step
+        })
+        data['steps-{}'.format(step)] = train(env, policy, 10000)
+
+    data = pd.DataFrame(data)
+    data = data.rolling(100).mean()
+    data.plot()
+    plt.show()
+
+
+def train(env: gym.Env, policy: Policy, episodes: int) -> np.ndarray:
+    rewards = np.zeros((episodes,))
 
     for i in tqdm(range(episodes), ncols=140, unit="episode"):
         rewards[i] = policy.train_episode(env)
-
-    data = pd.DataFrame({'rewards': rewards})
-    data = data.rolling(1000).mean()
-    data.plot()
-    plt.show()
+    return rewards
 
 
 if __name__ == '__main__':
