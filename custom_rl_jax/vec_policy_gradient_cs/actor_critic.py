28275a7f17d7cfefa6b52487b430b63d3827836f
Gabriel keith <gabe.keith@yahoo.com>
2023-12-30 18:56:54 -0500

    refactored actor critic

--- custom_rl_jax/vec_policy_gradient_cs/actor_critic.py
+++ custom_rl_jax/vec_policy_gradient_cs/actor_critic.py
@@ -2,13 +2,14 @@ import jax
 from jax import Array, random, numpy as jnp
 from jax.typing import ArrayLike
 from flax import linen as nn, struct
+from flax.core.frozen_dict import FrozenDict, Mapping
 from flax.struct import PyTreeNode
 import optax
 from typing import Any, TypedDict
 from functools import partial
 from .regularization import l2_regularization, l2_init_regularization, entropy_loss
 
-type ModelParams = dict[str, Any]
+type ModelParams = FrozenDict[str, Mapping[str, Any]] | dict[str, Any]
 
 
 Metrics = TypedDict(
@@ -25,81 +26,68 @@ Metrics = TypedDict(
 )
 
 
+class TrainingState(PyTreeNode):
+    init_actor_params: ModelParams
+    actor_params: ModelParams
+    actor_opt_state: optax.OptState
+
+    init_critic_params: ModelParams
+    critic_params: ModelParams
+    critic_opt_state: optax.OptState
+
+
 class ActorCritic(PyTreeNode):
-    debug: bool = struct.field(pytree_node=False)
     actor_model: nn.Module = struct.field(pytree_node=False)
-    critic_model: nn.Module = struct.field(pytree_node=False)
-
-    init_actor_params: ModelParams = struct.field()
-    init_critic_params: ModelParams = struct.field()
-    actor_params: ModelParams = struct.field()
-    critic_params: ModelParams = struct.field()
-
     actor_optimizer: optax.GradientTransformation = struct.field(pytree_node=False)
+    critic_model: nn.Module = struct.field(pytree_node=False)
     critic_optimizer: optax.GradientTransformation = struct.field(pytree_node=False)
-    actor_opt_state: optax.OptState = struct.field()
-    critic_opt_state: optax.OptState = struct.field()
-    importance: Array = struct.field()
 
     discount: float = struct.field(default=0.99)
+
+    actor_regularization_type: str = struct.field(pytree_node=False, default="l2_init")
+    actor_regularization_alpha: float = struct.field(default=0.001)
+    actor_entropy_regularization: float = struct.field(pytree_node=False, default=0.0)
 
     critic_regularization_type: str = struct.field(pytree_node=False, default="l2_init")
     critic_regularization_alpha: float = struct.field(default=0.001)
 
-    actor_regularization_type: str = struct.field(pytree_node=False, default="l2_init")
-    actor_regularization_alpha: float = struct.field(default=0.001)
-    actor_entropy_regularization: float = struct.field(pytree_node=False, default=0.000005)
-
-    @classmethod
     def init(
-        cls,
-        actor_model: nn.Module,
-        critic_model: nn.Module,
-        actor_optimizer: optax.GradientTransformation,
-        critic_optimizer: optax.GradientTransformation,
+        self,
         obs_space: int,
-        vec_num: int,
         key: ArrayLike,
-    ) -> tuple["ActorCritic", Array]:
+    ) -> tuple[TrainingState, Array]:
         key, actor_key, critic_key = random.split(key, 3)
         observation_dummy = jnp.zeros((obs_space,))
 
-        actor_params = actor_model.init(actor_key, observation_dummy)
-        critic_params = critic_model.init(critic_key, observation_dummy)
-        actor_opt_state = actor_optimizer.init(actor_params)
-        critic_opt_state = critic_optimizer.init(critic_params)
-
-        importance = jnp.ones((vec_num,))
+        actor_params = self.actor_model.init(actor_key, observation_dummy)
+        critic_params = self.critic_model.init(critic_key, observation_dummy)
+        actor_opt_state = self.actor_optimizer.init(actor_params)
+        critic_opt_state = self.critic_optimizer.init(critic_params)
 
         return (
-            cls(
-                debug=False,
-                actor_model=actor_model,
-                critic_model=critic_model,
+            TrainingState(
                 init_actor_params=actor_params,
-                init_critic_params=critic_params,
                 actor_params=actor_params,
-                critic_params=critic_params,
-                actor_optimizer=actor_optimizer,
-                critic_optimizer=critic_optimizer,
                 actor_opt_state=actor_opt_state,
+                init_critic_params=critic_params,
+                critic_params=critic_params,
                 critic_opt_state=critic_opt_state,
-                importance=importance,
             ),
             key,
         )
 
     @jax.jit
-    def act(self, obs: ArrayLike, key: ArrayLike) -> Array:
-        logits = self.actor_model.apply(self.actor_params, obs)
-        return random.categorical(key, logits)
+    def act(self, params: TrainingState, obs: ArrayLike, key: ArrayLike) -> tuple[Array, Array]:
+        key, action_key = random.split(key)
+        logits = self.actor_model.apply(params.actor_params, obs)
+        return random.categorical(action_key, logits), key
 
     @jax.jit
-    def vec_act(self, obs: ArrayLike, key: ArrayLike) -> tuple[Array, Array]:
+    def vec_act(self, params: TrainingState, obs: ArrayLike, key: ArrayLike) -> tuple[Array, Array]:
         key, action_key = random.split(key)
         vec_actor_model = jax.vmap(self.actor_model.apply, in_axes=(None, 0))
 
-        logits = vec_actor_model(self.actor_params, obs)
+        logits = vec_actor_model(params.actor_params, obs)
         actions = random.categorical(action_key, logits, axis=1)
         return actions, key
 
@@ -115,7 +103,7 @@ class ActorCritic(PyTreeNode):
 
     @jax.jit
     def critic_loss(
-        self, critic_params: ModelParams, obs: ArrayLike, expected_values: ArrayLike
+        self, critic_params: ModelParams, init_critic_params: ModelParams, obs: ArrayLike, expected_values: ArrayLike
     ) -> tuple[Array, Array]:
         critic_values = self.vec_state_values(critic_params, obs)
         loss = ((expected_values - critic_values) ** 2).mean()
@@ -123,12 +111,12 @@ class ActorCritic(PyTreeNode):
         reg_loss = 0.0
         if self.critic_regularization_type == "l2_init":
             reg_loss = l2_init_regularization(
-                self.critic_params,
-                self.init_critic_params,
+                critic_params,
+                init_critic_params,
                 alpha=self.critic_regularization_alpha,
             )
         elif self.critic_regularization_type == "l2":
-            reg_loss = l2_regularization(self.critic_params, alpha=self.critic_regularization_alpha)
+            reg_loss = l2_regularization(critic_params, alpha=self.critic_regularization_alpha)
 
         loss += reg_loss
 
@@ -136,20 +124,30 @@ class ActorCritic(PyTreeNode):
 
     @jax.jit
     def update_critic(
-        self, obs: ArrayLike, expected_values: ArrayLike
-    ) -> tuple[ModelParams, optax.OptState, Array, Array]:
+        self, params: TrainingState, obs: ArrayLike, expected_values: ArrayLike
+    ) -> tuple[TrainingState, Array, Array]:
         (loss, reg_loss), gradient = jax.value_and_grad(self.critic_loss, has_aux=True)(
-            self.critic_params, obs, expected_values
+            params.critic_params, params.init_critic_params, obs, expected_values
         )
-        updates, critic_opt_state = self.critic_optimizer.update(gradient, self.critic_opt_state, self.critic_params)
-        updated_params = optax.apply_updates(self.critic_params, updates)
+        updates, updated_opt_state = self.critic_optimizer.update(
+            gradient, params.critic_opt_state, params.critic_params
+        )
+        updated_params = optax.apply_updates(params.critic_params, updates)
 
-        return updated_params, critic_opt_state, loss, reg_loss
+        return (
+            params.replace(
+                critic_params=updated_params,
+                critic_opt_state=updated_opt_state,
+            ),
+            loss,
+            reg_loss,
+        )
 
     @jax.jit
     def actor_loss(
         self,
         actor_params: ModelParams,
+        init_actor_params: ModelParams,
         states: ArrayLike,
         actions: ArrayLike,
         advantages: ArrayLike,
@@ -160,9 +158,7 @@ class ActorCritic(PyTreeNode):
         loss = -jnp.mean(selected_action_prob * advantages)
 
         if self.actor_regularization_type == "l2_init":
-            reg_loss = l2_init_regularization(
-                actor_params, self.init_actor_params, alpha=self.actor_regularization_alpha
-            )
+            reg_loss = l2_init_regularization(actor_params, init_actor_params, alpha=self.actor_regularization_alpha)
         elif self.actor_regularization_type == "l2":
             reg_loss = l2_regularization(actor_params, alpha=self.actor_regularization_alpha)
         else:
@@ -178,53 +174,52 @@ class ActorCritic(PyTreeNode):
 
     @jax.jit
     def update_actor(
-        self, states: ArrayLike, actions: ArrayLike, advantages: ArrayLike
-    ) -> tuple[ModelParams, optax.OptState, Array, Array, Array]:
+        self, params: TrainingState, states: ArrayLike, actions: ArrayLike, advantages: ArrayLike
+    ) -> tuple[TrainingState, Array, Array, Array]:
         (loss, (reg_loss, entropy)), gradient = jax.value_and_grad(self.actor_loss, has_aux=True)(
-            self.actor_params, states, actions, advantages
+            params.actor_params, params.init_actor_params, states, actions, advantages
         )
-        updates, actor_opt_state = self.actor_optimizer.update(gradient, self.actor_opt_state, self.actor_params)
-        updated_params = optax.apply_updates(self.actor_params, updates)
-        return updated_params, actor_opt_state, loss, reg_loss, entropy
+        updates, updated_opt_state = self.actor_optimizer.update(gradient, params.actor_opt_state, params.actor_params)
+        updated_params = optax.apply_updates(params.actor_params, updates)
+        return (
+            params.replace(
+                actor_params=updated_params,
+                actor_opt_state=updated_opt_state,
+            ),
+            loss,
+            reg_loss,
+            entropy,
+        )
 
-    @partial(jax.jit, donate_argnums=(0,))
+    @partial(jax.jit, donate_argnums=(0, 1, 3, 7, 8))
     def vec_train_step(
         self,
+        params: TrainingState,
         obs: ArrayLike,
         action: ArrayLike,
-        next_obs: ArrayLike,
         rewards: ArrayLike,
+        next_obs: ArrayLike,
         done: ArrayLike,
+        importance: ArrayLike,
         key: ArrayLike,
-    ) -> tuple["ActorCritic", Array, Array, Metrics]:
-        expected_values = self.vec_state_values(self.critic_params, obs)
+    ) -> tuple[TrainingState, Metrics, Array, Array, Array]:
+        expected_values = self.vec_state_values(params.critic_params, obs)
 
         next_expected_values = jnp.where(
             done,
             rewards,
-            rewards + self.discount * jax.lax.stop_gradient(self.vec_state_values(self.critic_params, next_obs)),
+            rewards + self.discount * jax.lax.stop_gradient(self.vec_state_values(params.critic_params, next_obs)),
         )
 
         advantages = next_expected_values - expected_values
-        advantages *= self.importance
-        updated_critic_params, updated_critic_opt_state, c_loss, critic_reg_loss = self.update_critic(
-            obs, next_expected_values
-        )
-        updated_action_params, updated_actor_opt_state, a_loss, actor_reg_loss, entropy = self.update_actor(
-            obs, action, advantages
-        )
+        advantages *= importance
+
+        params, c_loss, critic_reg_loss = self.update_critic(params, obs, next_expected_values)
+        params, a_loss, actor_reg_loss, entropy = self.update_actor(params, obs, action, advantages)
 
         # set the importance back to 1 if it's the end of an episode
-        updated_importance = jnp.maximum(self.importance * self.discount, done)
-
-        updated_params = self.replace(
-            critic_params=updated_critic_params,
-            critic_opt_state=updated_critic_opt_state,
-            actor_params=updated_action_params,
-            actor_opt_state=updated_actor_opt_state,
-            importance=updated_importance,
-        )
-        actions, key = updated_params.vec_act(next_obs, key)
+        importance = jnp.maximum(importance * self.discount, done)
+        actions, key = self.vec_act(params, next_obs, key)
 
         metrics: Metrics = {
             "actor_loss": a_loss,
@@ -236,4 +231,4 @@ class ActorCritic(PyTreeNode):
             "critic_reg_loss": critic_reg_loss,
         }
 
-        return updated_params, actions, key, metrics
+        return params, metrics, importance, actions, key
--- custom_rl_jax/vec_policy_gradient_cs/cost_analysis.py
+++ custom_rl_jax/vec_policy_gradient_cs/cost_analysis.py
@@ -7,14 +7,9 @@ from .regularization import l2_regularization, l2_init_regularization, entropy_l
 
 
 def main():
-    vec_num = 1
+    vec_num = 4
     observation_space = 8
     action_space = 4
-
-    key = random.PRNGKey(53245)
-    key, actor_key, critic_key, action_key = random.split(key, 4)
-    observation = jnp.zeros((observation_space,))
-    vectorized_observation = jnp.zeros((vec_num, observation_space))
 
     actor_optimizer = optax.adam(2**-10, b1=0.97, b2=0.97)
     critic_optimizer = optax.adam(2**-9, b1=0.97, b2=0.97)
@@ -22,41 +17,52 @@ def main():
     actor_model = Mlp(features=[64, 64, action_space], last_layer_scale=0.01)
     critic_model = Mlp(features=[64, 64, 1], last_layer_scale=1.0)
 
-    actor_critic, key = ActorCritic.init(actor_model, critic_model, actor_optimizer, critic_optimizer, observation_space, vec_num, key)
+    actor_critic = ActorCritic(
+        actor_model=actor_model,
+        critic_model=critic_model,
+        actor_optimizer=actor_optimizer,
+        critic_optimizer=critic_optimizer,
+    )
 
+    key = random.PRNGKey(53245)
+    params, key = actor_critic.init(observation_space, key)
+
+    # dummy inputs
+    observation = jnp.zeros((observation_space,))
+    vectorized_observation = jnp.zeros((vec_num, observation_space))
     actions = jnp.zeros((vec_num,), dtype=jnp.int32)
     advantages = jnp.zeros((vec_num,))
 
-    #print(actor_critic.vec_act.lower(actor_critic, vectorized_observation, action_key).compiler_ir())
-
     lowered = {
-        "act": actor_critic.act.lower(actor_critic, observation, action_key),
-        "vectorized_act": actor_critic.vec_act.lower(actor_critic, vectorized_observation, action_key),
-        "action_log_prob": actor_critic.action_log_prob.lower(actor_critic, actor_critic.actor_params, observation),
-        "vectorized_state_value": actor_critic.vec_state_values.lower(
-            actor_critic, actor_critic.critic_params, vectorized_observation
-        ),
-        "l2_regularization": jax.jit(l2_regularization).lower(actor_critic.actor_params, 0.001),
-        "l2_init_regularization": jax.jit(l2_init_regularization).lower(
-            actor_critic.actor_params, actor_critic.actor_params, 0.001
-        ),
-        "entropy_loss": jax.jit(entropy_loss).lower(jnp.ones((vec_num, vec_num))),
-        "critic_loss": actor_critic.critic_loss.lower(
-            actor_critic, actor_critic.critic_params, vectorized_observation, advantages
-        ),
-        "update_critic": actor_critic.update_critic.lower(actor_critic, vectorized_observation, advantages),
-        "actor_loss": actor_critic.actor_loss.lower(
-            actor_critic, actor_critic.actor_params, vectorized_observation, actions, advantages
-        ),
-        "update_actor": actor_critic.update_actor.lower(actor_critic, vectorized_observation, actions, advantages),
+        # "act": actor_critic.act.lower(actor_critic, params, observation, key),
+        # "vectorized_act": actor_critic.vec_act.lower(actor_critic, params, vectorized_observation, key),
+        # "action_log_prob": actor_critic.action_log_prob.lower(actor_critic, params.actor_params, observation),
+        # "vectorized_state_value": actor_critic.vec_state_values.lower(
+        #     actor_critic, params.critic_params, vectorized_observation
+        # ),
+        # "l2_regularization": jax.jit(l2_regularization).lower(params.actor_params, 0.001),
+        # "l2_init_regularization": jax.jit(l2_init_regularization).lower(
+        #     params.actor_params, params.actor_params, 0.001
+        # ),
+        # "entropy_loss": jax.jit(entropy_loss).lower(jnp.ones((action_space, action_space))),
+        # "critic_loss": actor_critic.critic_loss.lower(
+        #     actor_critic, params.critic_params, params.init_critic_params, vectorized_observation, advantages
+        # ),
+        # "update_critic": actor_critic.update_critic.lower(actor_critic, params, vectorized_observation, advantages),
+        # "actor_loss": actor_critic.actor_loss.lower(
+        #     actor_critic, params.actor_params, params.init_actor_params, vectorized_observation, actions, advantages
+        # ),
+        # "update_actor": actor_critic.update_actor.lower(actor_critic, params, vectorized_observation, actions, advantages),
         "vectorized_train_step": actor_critic.vec_train_step.lower(
             actor_critic,
+            params,
             vectorized_observation,
             actions,
-            vectorized_observation,
             advantages,
+            vectorized_observation,
             jnp.zeros((vec_num,), dtype=jnp.bool_),
-            action_key,
+            jnp.ones((vec_num,)),
+            key,
         ),
     }
 
